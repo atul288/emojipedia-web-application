31cd39bc5281f641f9b33b6567f506501782a2d3
Atul Soni <atulkumarsoni2922@gmailcom>
2021-03-15 16:40:01 +0530

    Initial Commit

---
+++ src/emojipedia.js
@@ -0,0 +1,122 @@
+const emojipedia = [
+  {
+    id: 0,
+    emoji: "😀" ,
+    name: "Grinning Face",
+    meaning: "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text."
+  },
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 5,
+    emoji: "😁",
+    name: "Grinning Face With Smiling Eyes",
+    meaning: "Happy face with a mischievous laugh. Represents lightheartedness and exuberance. Full of joy due to an event, excited and agitated or just a bit embarrassed."
+  },
+  {
+   id: 6,
+   emoji: "😂",
+   name: "Face With Tears of Joy",
+   meaning: "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015"
+ },
+ {
+   id: 7,
+   emoji: "😆",
+   name: "Smiling Face With Open Mouth And Closed Eyes",
+   meaning: "Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person.. Means the same as “XD“ and “X“ as a text-based emoticon."
+ },
+ {
+   id: 8,
+   emoji: "😋",
+   name: "Face Savouring Delicious Food",
+   meaning: "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around."
+ },
+ {
+   id: 9,
+   emoji: "😉",
+   name: "Winking Face",
+   meaning: "Smiley blinks mischievously with one eye. Expression of humor, has been pulling a practical joke, is flirting with you or has an ulterior motive. The wink weakens the message: do not take it seriously, regard it as humorous."
+ },
+ {
+   id: 10,
+   emoji: "😍",
+   name: "Smiling Face With Heart-Eyes",
+   meaning: "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck."
+ },
+ {
+   id: 11,
+   emoji: "🥰",
+   name: "Smiling face with smiling eyes and three hearts",
+   meaning: "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart."
+ },
+ {
+   id: 12,
+   emoji: "😙",
+   name: "Kissing Face",
+   meaning: "The face sends kisses to another person. The open eyes and the neutral face are not so much a sign of an intimate kiss but rather a kiss to a friend or relative. “Thank you“, “Hello“ or “Goodbye“. Could also stand for “whistling” or “duck face”."
+ },
+ {
+   id: 13,
+   emoji: "🤗",
+   name: "Hugging Face",
+   meaning: "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+ },
+ {
+   id: 14,
+   emoji: "🤔",
+   name: "Thinking Face",
+   meaning: "The thinker's pose: Thoughtful face with the characteristic hand on the chin. Puts a statement, a person's intelligence, or an idea into question. Is mulling over something or thinking about a brilliant idea."
+ },
+ {
+   id: 15,
+   emoji: "😄",
+   name: "Smiling Face With Open Mouth And Smiling Eyes",
+   meaning: "Smiley's mouth is wide open, its eyes squeezed shut with joy. Laughs loudly, cheerfully and heartily. Only the typical emoji eyes distinguish it from the classical smiley face. Can also be used for sarcasm."
+ },
+ {
+   id: 16,
+   emoji: "😊",
+   name: "Smiling Face With Smiling Eyes",
+   meaning: "Is grinning shyly and embarrassed. The red cheeks are an expression of joy. Represents happiness, contentment, peace of mind and gratitude. Text-based variant: ^^."
+ },
+ {
+   id: 17,
+   emoji: "😎",
+   name: " Face With Sunglasses",
+   meaning: "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+ },
+ {
+   id: 18,
+   emoji: "😘",
+   name: "Face Throwing a Kiss",
+   meaning: "Smiley sends you a loving kiss that comes from the heart. Amicable, to express affection or with romantic intent, as an expression of love. May weaken a sarcastic response or be a reaction to a rude message. Symbol for flirting and showing gratitude."
+ }
+];
+
+export default emojipedia;
